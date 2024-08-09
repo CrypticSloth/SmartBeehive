bb9e0818f0e16f50b52e29d9706eb54c007a9c53
Erik Sorensen <sorensen.erik48@gmail.com>
2024-08-09 08:20:15 -0400

    ran overfit test on GPU

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Project to make my beehive smarter through data collection and observation.
 
 ## Install
 
-Package uses Ultralytics, which requires manual install of torch. Install here: https://pytorch.org/get-started/locally/
+Package uses Ultralytics, which requires manual install of torch. Install here: https://pytorch.org/get-started/locally/.
+
+On Windows for CUDA, also ensure all C++ redistributables are installed by installing the Visual Studio Community edition with the Desktop C++ worktable. Then Torch with CUDA will work.
--- yolo/train.py
+++ yolo/train.py
@@ -3,5 +3,12 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
-# Train the model
-results = model.train(data="dataset.yaml", epochs=100, imgsz=640)
+if __name__ == '__main__':
+    # Train the model
+    results = model.train(
+        data="dataset.yaml", 
+        epochs=100, 
+        imgsz=640, 
+        batch=-1, # Auto size based on 60% memory utilization, can set to float to customize memory utilization
+        device=0, # Set to train on GPU (1) or CPU (None)
+    )
